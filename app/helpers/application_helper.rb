21a42e8c6594da395992f1d18e595179e90f85cb
Brandon Levinger <brandon.levinger@gmail.com>
2012-08-27 07:43:15 -0700

    polish

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,12 +6,25 @@ module ApplicationHelper
   def menu
    [
        ['menu.home', root_path],
-       ['menu.news', news_index_path],
        ['menu.teams', teams_path],
        ['menu.coaches', coaches_path],
        ['menu.tournaments', tournaments_path],
        ['menu.referees', referees_path],
-       ['menu.calendar', calendar_path]
+       ['menu.news', news_index_path]
+    ].map do |item|
+      menu_item item, (@top_level_section_name == item[0]) ? 'current' : 'single-link'
+   end
+   .join('')
+   .html_safe
+  end
+	
+	def menu_secondary
+   [
+       ['menu.calendar', calendar_path],
+       ['menu.newsletter', root_path],
+       ['menu.facebook', root_path],
+       ['menu.clubInfo', root_path],
+       ['menu.contact', root_path]
     ].map do |item|
       menu_item item, (@top_level_section_name == item[0]) ? 'current' : 'single-link'
    end
@@ -19,11 +32,13 @@ module ApplicationHelper
    .html_safe
   end
 
+
+
   private
 
   LANGUAGE_MENU_ITEM = ['menu.language', "http://#{AppConfig[:switch_hosts][I18n.locale]}"]
 
   def menu_item item, class_name
-    content_tag :li, link_to(I18n.t(item[0]), item[1]), :class => class_name
+    content_tag :li, class_name == 'current' ? content_tag(:span, I18n.t(item[0])) : link_to(I18n.t(item[0]), item[1]), :class => class_name
   end
 end
