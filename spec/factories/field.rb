1b321ac825de0490802ea643386b48d327529d24
Ryan Robinett <ryan@digitalfoundry.com>
2012-10-30 17:59:33 -0700

    updated field spec to use factory

---
+++ spec/factories/field.rb
@@ -0,0 +1,14 @@
+# spec/factories/field.rb
+require 'faker'
+
+FactoryGirl.define do
+  factory :field do |f|
+    f.name { Faker::Company.name }
+    f.club { Faker::Company.catch_phrase }
+    f.rain_line { Faker::PhoneNumber.phone_number }
+    f.state_id { rand(3) }
+    f.lat { Faker::Address.latitude }
+    f.lng { Faker::Address.longitude }
+    f.address { Faker::Address.street_address }
+  end
+end
--- spec/models/field_spec.rb
+++ spec/models/field_spec.rb
@@ -17,36 +17,27 @@
 require 'spec_helper'
 
 describe Field do
-  before(:each) do
-    @attr = { :name => "Sample Site", :club => "Sample Club", :rain_line => "234-2344", :address => "335 Franklin School RoadFort Kent, ME 04743", :state_id => 1}
+  it "has a valid factory" do
+    FactoryGirl.create(:field)
   end
 
-  it "should create a field given valid attributes" do
-    Field.create!(@attr)
+  it "requires a name" do
+    FactoryGirl.build(:field, name: nil).should_not be_valid
   end
 
-  it "should require a name" do
-    no_name_field = Field.new(@attr.merge(:name => ""))
-    no_name_field.should_not be_valid
+  it "requires a club" do
+    FactoryGirl.build(:field, club: nil).should_not be_valid
   end
 
-  it "should require a club" do
-    no_club_field = Field.new(@attr.merge(:club => ""))
-    no_club_field.should_not be_valid
+  it "requires a rain line" do
+    FactoryGirl.build(:field, rain_line: nil).should_not be_valid
   end
 
-  it "should require a rain line" do
-    no_rain_line_field = Field.new(@attr.merge(:rain_line => ""))
-    no_rain_line_field.should_not be_valid
-  end
-
-  it "should require an address" do
-    no_map_url_field = Field.new(@attr.merge(:address => ""))
-    no_map_url_field.should_not be_valid
+  it "requires an address" do
+    FactoryGirl.build(:field, address: nil).should_not be_valid
   end
 
   it "should require a state id" do
-    no_state_id_field = Field.new(@attr.merge(:state_id => nil))
-    no_state_id_field.should_not be_valid
+    FactoryGirl.build(:field, state_id: nil).should_not be_valid
   end
 end
