27831173eadf83948a40771f984782f6abecdf44
Ryan Robinett <ryan@digitalfoundry.com>
2012-11-05 09:09:08 -0800

    removed parent and player roles until needed in the future

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@
 class User < ActiveRecord::Base
   include Rails.application.routes.url_helpers # needed for _path helpers to work in models
 
-  ROLES = %w[admin board_member team_manager field_manager parent player]
+  ROLES = %w[admin board_member team_manager field_manager]
 
   has_paper_trail
 
