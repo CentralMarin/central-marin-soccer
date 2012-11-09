6b72ff35ecee8b4445530bd5ec254eed1a93a406
Ryan Robinett <ryan@digitalfoundry.com>
2012-11-09 11:32:08 -0800

    Now use simplemodal instead of jquerytools

--- app/assets/javascripts/coaches.js
+++ app/assets/javascripts/coaches.js
@@ -4,13 +4,10 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //
-//= require jquery
-//= require jquery_ujs
-//= require jquery-ui
-//= require jquery.tools.min.js
 //= require plugins
 //= require spin
 //= require jsrender
+//= require jquery.simplemodal.1.4.3.min
 
 
     var spinner = new Spinner({
@@ -24,35 +21,16 @@
         shadow: true // Whether to render a shadow
     });
 
-    $('a[rel]').overlay({
-        effect: 'apple',
-        onBeforeLoad: function(event) {
-            var element = event.srcElement;
-            while (element.tagName != 'A' && element != null) {
-                element = element.parentElement;
-            }
+    $('a[rel]').click(function(event) {
 
-            // get the overlay's content div
-            var overlay = this.getOverlay();
+        var coachId = event.currentTarget.dataset['coachId'];
+        $.getJSON('/coaches/' + coachId + '.json', function(coachDetails) {
+            coachDetails.coachImageUrl = $('#coach-img-' + coachId)[0].src;
+            $.modal($("#CoachTemplate").render(coachDetails));
+        });
 
-            // determine if we've already populated the overlay
-            //if (overlay.children.length == 0) {
-
-                var coachId = element.dataset['coachId'];
-
-                // build the url with the coach id
-                var url = '/coaches/' + coachId + '.json';
-
-                $.getJSON(url, function(coachDetails) {
-
-                    // get the url for the coach image
-                    coachDetails.coachImageUrl = $('#coach-img-' + coachId )[0].src;
-
-                    var content = overlay.find('div.contentWrap');
-                    content.html($("#CoachTemplate").render(coachDetails));
-                });
-            //}
-        }
+        return false;
     });
+
 
     $('#accordion').accordion();
