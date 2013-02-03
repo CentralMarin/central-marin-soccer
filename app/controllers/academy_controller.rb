c0bfe7acfd98e2329f60a1f3c92c56f8d91b6cb8
Ryan Robinett <ryan@digitalfoundry.com>
2013-02-02 22:29:08 -0800

    Updated program information

--- app/controllers/academy_controller.rb
+++
@@ -1,5 +0,0 @@
-class AcademyController < ApplicationController
-  def index
-    @academy_teams = Team.academy(Time.now.year)
-  end
-end
--- app/controllers/information_controller.rb
+++ app/controllers/information_controller.rb
@@ -1,4 +1,23 @@
 class InformationController < ApplicationController
   def index
   end
+
+  def gold
+    @level = 'gold'
+    # Get the bullets
+    @bullets = I18n.t("gold.bullets")
+
+    render :action => 'level'
+  end
+
+  def silver
+    @level = 'silver'
+    # Get the bullets
+    @bullets = I18n.t("silver.bullets")
+    render :action => 'level'
+  end
+
+  def academy
+    @academy_teams = Team.academy(Time.now.year)
+  end
 end
