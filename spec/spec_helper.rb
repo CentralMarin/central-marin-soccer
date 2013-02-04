25ca6739790c358ad15985747aadd6262c7e44ef
Ryan Robinett <ryan@digitalfoundry.com>
2013-02-04 13:43:16 -0800

    Updated to allow debugging of specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,13 +7,12 @@ Spork.prefork do
   # if you change any configuration or code from libraries loaded here, you'll
   # need to restart spork for it take effect.
 
-  # Delay loading route loading so spork works with ActiveAdmin
-  Spork.trap_method(Rails::Application::RoutesReloader, :reload!) # Rails 3.1
-
   # This file is copied to spec/ when you run 'rails generate rspec:install'
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
-#  require 'capybara/rspec'
+
+  # Delay loading route loading so spork works with ActiveAdmin
+  Spork.trap_method(Rails::Application::RoutesReloader, :reload!) # Rails 3.1
 
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
