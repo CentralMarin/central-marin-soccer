e3fbdb774d304d7ddb17834cd44902d44b0e1f5d
Ryan Robinett <ryan@digitalfoundry.com>
2013-02-06 08:30:13 -0800

    Now works with Dashboard

--- app/models/web_part.rb
+++ app/models/web_part.rb
@@ -1,4 +1,6 @@
 class WebPart < ActiveRecord::Base
+  include Rails.application.routes.url_helpers # needed for _path helpers to work in models
+
   attr_accessible :html, :name
 
   translates :html, versioning: true, fallbacks_for_empty_translations: true
@@ -6,9 +8,18 @@ class WebPart < ActiveRecord::Base
   validates :name, :presence => true, :uniqueness => true
   validates :html, :presence => true
 
+  def admin_permalink
+    admin_web_part_path(self)
+  end
+
   class Translation
+    include Rails.application.routes.url_helpers # needed for _path helpers to work in models
 
     attr_accessible :html
+
+    def admin_permalink
+      admin_web_part_path(self)
+    end
 
     def to_s
       web_part = WebPart.find(self['web_part_id'])
