e5501faf8e6ff4e75263ec7fe591c06285b787ef
Ryan Robinett <ryan@digitalfoundry.com>
2013-02-07 20:56:38 -0800

    Added content for information pages

--- app/controllers/information_controller.rb
+++ app/controllers/information_controller.rb
@@ -1,12 +1,19 @@
 class InformationController < ApplicationController
-  def index
-
+  def init_web_parts(web_parts)
     # Determine if the current user can edit the page
     @editable = can?(:manage, WebPart)
-    @web_parts = WebPart.load('information.overview')
+    @web_parts = WebPart.load(web_parts)
+  end
+
+  def index
+    @part_name = 'information.overview'
+    init_web_parts(@part_name)
   end
 
   def gold
+    @part_name = 'information.gold'
+    init_web_parts(@part_name)
+
     @level = 'gold'
     # Get the bullets
     @bullets = I18n.t("gold.bullets")
@@ -15,6 +22,10 @@ class InformationController < ApplicationController
   end
 
   def silver
+    @part_name = 'information.silver'
+    init_web_parts(@part_name)
+
+
     @level = 'silver'
     # Get the bullets
     @bullets = I18n.t("silver.bullets")
@@ -23,10 +34,16 @@ class InformationController < ApplicationController
   end
 
   def academy
+    @part_name = 'information.academy'
+    init_web_parts(@part_name)
+
     @academy_teams = Team.academy(Time.now.year)
   end
 
   def scholarship
+    @part_name = 'information.scholarship'
+    init_web_parts(@part_name)
+
     # load translations
     @goals = I18n.t("scholarship.goal_text")
     @history = I18n.t("scholarship.history_bullets")
