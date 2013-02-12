893b5d5b36e76f7946aa7e5d160c03758e472c39
Ryan Robinett <ryan@digitalfoundry.com>
2013-02-12 15:29:21 -0800

    added test

---
+++ spec/features/teams_active_admins_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+feature "TeamsActiveAdmins" do
+
+  context "As an administrator" do
+
+    before do
+      login_as_admin
+    end
+
+    it "the admin teams view renders" do
+
+      visit admin_teams_path
+      assert current_path == admin_teams_path
+    end
+
+    it "he should create a new team", :js => true do
+
+      team = FactoryGirl.build(:team)
+
+      visit admin_teams_path
+      click_link "New Team"
+
+      select team.coach.name, :from => "team[coach_id]"
+      select team.team_level.name, :from => "team[team_level_id]"
+      fill_in "Year", :with => team.year
+      select team.gender, :from => "team[gender]"
+      fill_in "Name", :with => team.name
+      fill_in "Manager name", :with => team.manager_name
+      fill_in "Manager email", :with => team.manager_email
+      fill_in "Manager phone", :with => team.manager_phone
+      fill_in "Teamsnap url", :with => team.teamsnap_url
+
+      click_button "Create Team"
+
+      assert_path admin_team_path(Team.last)
+
+      page.should have_content(team.name)
+
+    end
+  end
+end
