342d60c093ebafc6cadf30137bd6ed8ce2fd8ef7
Ryan Robinett <ryan@digitalfoundry.com>
2013-02-28 15:58:47 -0800

    Cleaned up JavaScript

--- app/assets/javascripts/information.tournaments.js
+++ app/assets/javascripts/information.tournaments.js
@@ -1,21 +1,35 @@
 //= require jquery.simplemodal.1.4.4.min
 //= require jsrender
+//= require namespace
 
-$(document).ready(function() {
-    $("#tabs").tabs().addClass( "ui-tabs-vertical ui-helper-clearfix" );
-    $( "#tabs li" ).removeClass( "ui-corner-top" ).addClass( "ui-corner-left" );
+(function () {
+    namespace("soccer.information");
 
-    $('a[rel]').click(function(event) {
-        var tournament = event.currentTarget.dataset['tournament'];
-        var year = event.currentTarget.dataset['year'];
-        $.getJSON(document.URL + '/' + tournament + '/' + year + '.json', function(pastWinners) {
-            var modal = $.modal($("#pastWinnersTemplate").render(pastWinners));
+    soccer.information.tournaments = function() {
 
-            soccer.inline_editing().enable($('#simplemodal')[0]);
-        });
+        var init = function() {
+            $(document).ready(function() {
+                $("#tabs").tabs().addClass( "ui-tabs-vertical ui-helper-clearfix" );
+                $( "#tabs li" ).removeClass( "ui-corner-top" ).addClass( "ui-corner-left" );
 
-        return false;
-    });
+                $('a[rel]').click(function(event) {
+                    var tournament = event.currentTarget.dataset['tournament'];
+                    var year = event.currentTarget.dataset['year'];
+                    $.getJSON(document.URL + '/' + tournament + '/' + year + '.json', function(pastWinners) {
+                        var modal = $.modal($("#pastWinnersTemplate").render(pastWinners));
 
-});
+                        soccer.inline_editing().enable($('#simplemodal')[0]);
+                    });
 
+                    return false;
+                });
+            });
+        };
+
+        return {
+            init:init
+        };
+    };
+}());
+
+soccer.information.tournaments().init();
