1815116b05380a9c0e5bd90e67f3030dddbf83c8
Ryan Robinett <ryan@digitalfoundry.com>
2014-08-24 08:55:45 -0700

    Fixed locales

--- config/application.rb
+++ config/application.rb
@@ -18,8 +18,8 @@ module CentralMarinSoccer
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-# config.i18n.available_locales = [:en, :es]
-# config.i18n.default_locale = :en
+    config.i18n.available_locales = [:en, :es]
+    config.i18n.default_locale = :en
     config.i18n.fallbacks = true
 
 
