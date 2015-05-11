c84e50962486a9d1f9bdb04888e868096cefef20
Ryan Robinett <ryan@digitalfoundry.com>
2015-05-11 10:13:45 -0700

    Removed old tryouts

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150402163152) do
+ActiveRecord::Schema.define(version: 20150511170854) do
 
-  create_table "active_admin_comments", force: true do |t|
-    t.string   "namespace"
+  create_table "active_admin_comments", force: :cascade do |t|
+    t.string   "namespace",     limit: 255
     t.text     "body"
-    t.string   "resource_id",   null: false
-    t.string   "resource_type", null: false
+    t.string   "resource_id",   limit: 255, null: false
+    t.string   "resource_type", limit: 255, null: false
     t.integer  "author_id"
-    t.string   "author_type"
+    t.string   "author_type",   limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -28,17 +28,17 @@ ActiveRecord::Schema.define(version: 20150402163152) do
   add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace"
   add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
 
-  create_table "admin_users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+  create_table "admin_users", force: :cascade do |t|
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                      default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -46,41 +46,41 @@ ActiveRecord::Schema.define(version: 20150402163152) do
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
 
-  create_table "article_carousels", force: true do |t|
+  create_table "article_carousels", force: :cascade do |t|
     t.integer "article_id",     null: false
     t.integer "carousel_order", null: false
   end
 
-  create_table "article_translations", force: true do |t|
-    t.integer  "article_id", null: false
-    t.string   "locale",     null: false
+  create_table "article_translations", force: :cascade do |t|
+    t.integer  "article_id",             null: false
+    t.string   "locale",     limit: 255, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "title"
+    t.string   "title",      limit: 255
     t.text     "body"
   end
 
   add_index "article_translations", ["article_id"], name: "index_article_translations_on_article_id"
   add_index "article_translations", ["locale"], name: "index_article_translations_on_locale"
 
-  create_table "articles", force: true do |t|
-    t.string   "title"
+  create_table "articles", force: :cascade do |t|
+    t.string   "title",       limit: 255
     t.text     "body"
-    t.string   "author"
-    t.string   "image"
+    t.string   "author",      limit: 255
+    t.string   "image",       limit: 255
     t.integer  "category_id"
     t.integer  "team_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "published",   default: false
+    t.boolean  "published",               default: false
     t.integer  "coach_id"
   end
 
   add_index "articles", ["team_id"], name: "index_articles_on_team_id"
 
-  create_table "ckeditor_assets", force: true do |t|
-    t.string   "data_file_name",               null: false
-    t.string   "data_content_type"
+  create_table "ckeditor_assets", force: :cascade do |t|
+    t.string   "data_file_name",    limit: 255, null: false
+    t.string   "data_content_type", limit: 255
     t.integer  "data_file_size"
     t.integer  "assetable_id"
     t.string   "assetable_type",    limit: 30
@@ -92,9 +92,9 @@ ActiveRecord::Schema.define(version: 20150402163152) do
   add_index "ckeditor_assets", ["assetable_type", "assetable_id"], name: "idx_ckeditor_assetable"
   add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type"
 
-  create_table "coach_translations", force: true do |t|
-    t.integer  "coach_id",   null: false
-    t.string   "locale",     null: false
+  create_table "coach_translations", force: :cascade do |t|
+    t.integer  "coach_id",               null: false
+    t.string   "locale",     limit: 255, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "bio"
@@ -103,23 +103,23 @@ ActiveRecord::Schema.define(version: 20150402163152) do
   add_index "coach_translations", ["coach_id"], name: "index_coach_translations_on_coach_id"
   add_index "coach_translations", ["locale"], name: "index_coach_translations_on_locale"
 
-  create_table "coaches", force: true do |t|
-    t.string   "name"
-    t.string   "email"
+  create_table "coaches", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "email",      limit: 255
     t.text     "bio"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "image"
+    t.string   "image",      limit: 255
   end
 
   add_index "coaches", ["email"], name: "index_coaches_on_email", unique: true
 
-  create_table "contact_translations", force: true do |t|
-    t.integer  "contact_id",    null: false
-    t.string   "locale",        null: false
+  create_table "contact_translations", force: :cascade do |t|
+    t.integer  "contact_id",                null: false
+    t.string   "locale",        limit: 255, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "club_position"
+    t.string   "club_position", limit: 255
     t.text     "description"
     t.text     "bio"
   end
@@ -127,20 +127,20 @@ ActiveRecord::Schema.define(version: 20150402163152) do
   add_index "contact_translations", ["contact_id"], name: "index_contact_translations_on_contact_id"
   add_index "contact_translations", ["locale"], name: "index_contact_translations_on_locale"
 
-  create_table "contacts", force: true do |t|
-    t.string   "name"
-    t.string   "email"
+  create_table "contacts", force: :cascade do |t|
+    t.string   "name",          limit: 255
+    t.string   "email",         limit: 255
     t.text     "bio"
-    t.string   "club_position"
+    t.string   "club_position", limit: 255
     t.text     "description"
     t.integer  "category"
     t.integer  "row_order"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "image"
+    t.string   "image",         limit: 255
   end
 
-  create_table "event_details", force: true do |t|
+  create_table "event_details", force: :cascade do |t|
     t.datetime "start"
     t.integer  "duration"
     t.integer  "location_id"
@@ -149,21 +149,21 @@ ActiveRecord::Schema.define(version: 20150402163152) do
     t.datetime "updated_at"
   end
 
-  create_table "event_groups", force: true do |t|
+  create_table "event_groups", force: :cascade do |t|
     t.integer  "event_id"
-    t.integer  "groups",          default: 0
-    t.string   "boys_age_range"
-    t.string   "girls_age_range"
+    t.integer  "groups",                      default: 0
+    t.string   "boys_age_range",  limit: 255
+    t.string   "girls_age_range", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "event_translations", force: true do |t|
-    t.integer  "event_id",   null: false
-    t.string   "locale",     null: false
+  create_table "event_translations", force: :cascade do |t|
+    t.integer  "event_id",               null: false
+    t.string   "locale",     limit: 255, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "heading"
+    t.string   "heading",    limit: 255
     t.text     "body"
     t.text     "tout"
   end
@@ -171,118 +171,86 @@ ActiveRecord::Schema.define(version: 20150402163152) do
   add_index "event_translations", ["event_id"], name: "index_event_translations_on_event_id"
   add_index "event_translations", ["locale"], name: "index_event_translations_on_locale"
 
-  create_table "events", force: true do |t|
-    t.integer  "type",       default: 0, null: false
-    t.string   "heading"
+  create_table "events", force: :cascade do |t|
+    t.integer  "type",                   default: 0, null: false
+    t.string   "heading",    limit: 255
     t.text     "body"
     t.text     "tout"
-    t.integer  "status",     default: 0, null: false
+    t.integer  "status",                 default: 0, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "locations", force: true do |t|
-    t.string   "name"
-    t.string   "club"
-    t.string   "rain_line"
+  create_table "locations", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "club",       limit: 255
+    t.string   "rain_line",  limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "status"
-    t.decimal  "lat",        precision: 15, scale: 10
-    t.decimal  "lng",        precision: 15, scale: 10
-    t.string   "address"
-    t.string   "type",                                 default: "Field"
+    t.decimal  "lat",                    precision: 15, scale: 10
+    t.decimal  "lng",                    precision: 15, scale: 10
+    t.string   "address",    limit: 255
+    t.string   "type",       limit: 255,                           default: "Field"
   end
 
-  create_table "team_level_translations", force: true do |t|
-    t.integer  "team_level_id", null: false
-    t.string   "locale",        null: false
+  create_table "team_level_translations", force: :cascade do |t|
+    t.integer  "team_level_id",             null: false
+    t.string   "locale",        limit: 255, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "name"
+    t.string   "name",          limit: 255
   end
 
   add_index "team_level_translations", ["locale"], name: "index_team_level_translations_on_locale"
   add_index "team_level_translations", ["team_level_id"], name: "index_team_level_translations_on_team_level_id"
 
-  create_table "team_levels", force: true do |t|
-    t.string   "name"
+  create_table "team_levels", force: :cascade do |t|
+    t.string   "name",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "teams", force: true do |t|
+  create_table "teams", force: :cascade do |t|
     t.integer  "year"
     t.integer  "gender_id",        limit: 255
-    t.string   "name"
+    t.string   "name",             limit: 255
     t.integer  "coach_id"
     t.integer  "team_level_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "image"
-    t.string   "teamsnap_team_id"
+    t.string   "image",            limit: 255
+    t.string   "teamsnap_team_id", limit: 255
   end
 
-  create_table "tryout_registrations", force: true do |t|
-    t.string   "first"
-    t.string   "last"
-    t.string   "home_address"
-    t.string   "home_phone"
+  create_table "tryout_registrations", force: :cascade do |t|
+    t.string   "first",         limit: 255
+    t.string   "last",          limit: 255
+    t.string   "home_address",  limit: 255
+    t.string   "home_phone",    limit: 255
     t.date     "birthdate"
     t.integer  "age"
     t.integer  "gender"
-    t.string   "previous_team"
-    t.string   "parent1_first"
-    t.string   "parent1_last"
-    t.string   "parent1_cell"
-    t.string   "parent1_email"
-    t.string   "parent2_first"
-    t.string   "parent2_last"
-    t.string   "parent2_cell"
-    t.string   "parent2_email"
-    t.string   "completed_by"
-    t.string   "relationship"
+    t.string   "previous_team", limit: 255
+    t.string   "parent1_first", limit: 255
+    t.string   "parent1_last",  limit: 255
+    t.string   "parent1_cell",  limit: 255
+    t.string   "parent1_email", limit: 255
+    t.string   "parent2_first", limit: 255
+    t.string   "parent2_last",  limit: 255
+    t.string   "parent2_cell",  limit: 255
+    t.string   "parent2_email", limit: 255
+    t.string   "completed_by",  limit: 255
+    t.string   "relationship",  limit: 255
     t.boolean  "waiver"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "city"
+    t.string   "city",          limit: 255
   end
 
-  create_table "tryout_type_translations", force: true do |t|
-    t.integer  "tryout_type_id", null: false
-    t.string   "locale",         null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "header"
-    t.text     "body"
-  end
-
-  add_index "tryout_type_translations", ["locale"], name: "index_tryout_type_translations_on_locale"
-  add_index "tryout_type_translations", ["tryout_type_id"], name: "index_tryout_type_translations_on_tryout_type_id"
-
-  create_table "tryout_types", force: true do |t|
-    t.string   "name"
-    t.string   "header"
-    t.text     "body"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.boolean  "show"
-  end
-
-  create_table "tryouts", force: true do |t|
-    t.integer  "gender_id"
-    t.integer  "age"
-    t.datetime "start"
-    t.integer  "field_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "duration"
-    t.integer  "tryout_type_id"
-  end
-
-  create_table "web_part_translations", force: true do |t|
-    t.integer  "web_part_id", null: false
-    t.string   "locale",      null: false
+  create_table "web_part_translations", force: :cascade do |t|
+    t.integer  "web_part_id",             null: false
+    t.string   "locale",      limit: 255, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "html"
@@ -291,8 +259,8 @@ ActiveRecord::Schema.define(version: 20150402163152) do
   add_index "web_part_translations", ["locale"], name: "index_web_part_translations_on_locale"
   add_index "web_part_translations", ["web_part_id"], name: "index_web_part_translations_on_web_part_id"
 
-  create_table "web_parts", force: true do |t|
-    t.string   "name"
+  create_table "web_parts", force: :cascade do |t|
+    t.string   "name",       limit: 255
     t.text     "html",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
