df6dda02634cd9b24e0f11f0aa58dd11f955a167
Ryan Robinett <ryan@digitalfoundry.com>
2015-06-07 15:51:27 -0700

    Moved lat long saving to base class

--- app/models/field.rb
+++ app/models/field.rb
@@ -26,8 +26,6 @@ class Field < Location
   validates :address, :presence => true
   validates :status, :presence => true
 
-  before_save :set_latlng
-
   has_many :event_details
 
   default_scope { order('name asc') }
@@ -56,30 +54,5 @@ class Field < Location
     name
   end
 
-  protected
-
-  def set_latlng
-
-    # geocode the address
-    uri = URI.parse("http://maps.googleapis.com/maps/api/geocode/json?address=#{CGI.escape(address)},+CA&sensor=false")
-
-    # Shortcut
-    response = Net::HTTP.get_response(uri)
-    if (response.code != '200')
-      errors.add(:base, "Unable to convert address to latitude and longitude - HTTP response #{response.code}" )
-      return false
-    end
-
-    json = JSON.parse(response.body)
-    if (json['status'] != 'OK')
-      errors.add(:base, "Unable to convert address to latitude and longitude - JSON status #{json['status']}")
-      return false
-    end
-
-    location = json['results'][0]['geometry']['location']
-
-    self.lat = location['lat']
-    self.lng = location['lng']
-  end
 
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,35 @@
 class Location < ActiveRecord::Base
+
+  before_save :set_latlng
+
+  def map_url
+    "http://maps.google.com/maps?q=#{lat},#{lng}&iwloc=A"
+  end
+
+  protected
+
+  def set_latlng
+
+    # geocode the address
+    uri = URI.parse("http://maps.googleapis.com/maps/api/geocode/json?address=#{CGI.escape(address)},+CA&sensor=false")
+
+    # Shortcut
+    response = Net::HTTP.get_response(uri)
+    if (response.code != '200')
+      errors.add(:base, "Unable to convert address to latitude and longitude - HTTP response #{response.code}" )
+      return false
+    end
+
+    json = JSON.parse(response.body)
+    if (json['status'] != 'OK')
+      errors.add(:base, "Unable to convert address to latitude and longitude - JSON status #{json['status']}")
+      return false
+    end
+
+    location = json['results'][0]['geometry']['location']
+
+    self.lat = location['lat']
+    self.lng = location['lng']
+  end
+
 end
