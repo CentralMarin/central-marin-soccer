1f5c52569e47a2e81a60bcbe07faeec99ba24d23
Ryan Robinett <ryan@digitalfoundry.com>
2015-06-16 10:43:41 -0700

    Streamlined deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,8 +3,7 @@ lock '3.4.0'
 
 config = YAML.load_file('config/secrets.yml')
 
-#server '207.104.28.18', user: "#{config['development']['server_user']}",roles: %w{app, web, db}
-server '192.168.1.17', user: "#{config['development']['server_user']}",roles: %w{app, web, db}
+server '207.104.28.18', user: "#{config['development']['server_user']}",roles: %w{app, web, db}
 
 # Source Control
 set :application, 'centralmarinsoccer'
@@ -20,15 +19,6 @@ set :deploy_to, "/webapps/centralmarinsoccer"
 
 set :linked_files, %w{config/secrets.yml config/CentralMarinSoccerTryouts.p12}
 set :linked_dirs, %w{public/uploads public/assets public/system log}
-#
-#set :ssh_options, {
-#    verbose: :debug
-#}
-
-##setup whenever for cron support
-##require "whenever/capistrano"
-##set :whenever_environment, #{stage}
-##set :whenever_identifier, "#{application}_#{stage}"
 
 # Default value for keep_releases is 5
 set :keep_releases, 5
@@ -36,20 +26,6 @@ set :keep_releases, 5
 SSHKit.config.command_map[:rake] = "bundle exec rake"
 
 namespace :deploy do
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
-
-  #desc 'Symbolic linking uploads directory'
-  #task :symlink_uploads do
-  #  run "mkdir -p #{shared_path}/uploads"
-  #  run "#{try_sudo} chmod 777 #{shared_path}/uploads"
-  #  run "ln -nfs #{shared_path}/uploads  #{release_path}/public/uploads"
-  #end
-
   desc "Create initial folder structure"
   task :setup do
     run "mkdir -p #{shared_path}/config"
@@ -61,15 +37,6 @@ namespace :deploy do
   # remove old releases
   after :deploy, :cleanup
 
-  after :publishing, :restart
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
+  # restart app server
+  after :deploy, :'passenger:restart'
 end
