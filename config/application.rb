9de5e84caf400a6ddc25217d3fbc7740c63457d3
Ryan Robinett <ryan@digitalfoundry.com>
2015-08-25 15:52:38 -0700

    Removed active admin warnings

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,9 @@ module CentralMarinSoccer
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    # Opt in to new ActiveRecord transaction behavior
+    config.active_record.raise_in_transactional_callbacks = true
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
 # config.time_zone = 'Pacific Time (US & Canada)'
 
