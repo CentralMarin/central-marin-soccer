23973addb9f787335be3ca8046581b8ba246699f
Ryan Robinett <ryan@digitalfoundry.com>
2015-08-26 18:08:52 -0700

    Created CMS base controller class

--- app/controllers/information_controller.rb
+++ app/controllers/information_controller.rb
@@ -1,16 +1,4 @@
-class InformationController < ApplicationController
-
-  def init_web_parts(page_name)
-
-    page = Page.find_by(name: page_name)
-
-    part_name = []
-    part_name = page.web_parts.map {|part| part.name } unless page.nil?
-    @web_parts = WebPart.load(part_name)
-
-    part_name = part_name[0] if part_name.length == 1
-    part_name
-  end
+class InformationController < CmsController
 
   def index
     init_web_parts('Information')
