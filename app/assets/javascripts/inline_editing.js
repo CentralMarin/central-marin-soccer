da542534ac1f368a88e017fa65d6a1b512a158ff
Ryan Robinett <ryan@digitalfoundry.com>
2015-09-01 09:40:41 -0700

    Now cleans up CKEDitors

--- app/assets/javascripts/inline_editing.js
+++ app/assets/javascripts/inline_editing.js
@@ -6,18 +6,13 @@
 
         var tabsHTML = "<ul><li><a href='#tabs-english'>English</a></li><li><a href='#tabs-spanish'>Spanish</a></li></ul><div id='tabs-spanish'><p>SPANISH</p></div><span style='position: absolute; right: 5px; top: 10px'><a href='#' id='save'>Save</a><a href='#' id='cancel'>Cancel</a></a></span>";
 
-        var enable_inline = function(elem) {
+        var _enable_inline = function(elem) {
             var $elem = $(elem);
 
             $elem.click(function(event) {
                 _addTabs(elem);
                 console.log('Focus in');
                 event.stopPropagation();
-            });
-
-            $elem.focusout(function() {
-                //_removeTags(elem);
-               console.log('Focus out');
             });
         };
 
@@ -38,18 +33,19 @@
 
         var _cancelEdit = function(webPartName, elem) {
             // Reload the original saved content
-            _loadContent('en', webPartName, elem);
             _removeTabs();
+
+            _loadContent('en', webPartName, elem, false);
         };
 
-        var _loadContent = function(locale, name, elem) {
+        var _loadContent = function(locale, name, elem, editor) {
             $.get('/web_part/' + locale, {name: name} )
                 .done(function(data) {
                     // Update the element
                     elem.innerHTML = data.html;
 
                     // Enable CKEditor
-                    _showCKEditor(elem);
+                    if (editor) _showCKEditor(elem);
 
                 } )
                 .fail(function(jqXHR, status, error) { alert("Error: " + status + " " + error)})
@@ -88,8 +84,8 @@
 
             // Reload content to make sure we have the latest
             var webPartName = $(elem).data('name');
-            _loadContent('en', webPartName, elem);
-            _loadContent('es', webPartName, es_elem);
+            _loadContent('en', webPartName, elem, true);
+            _loadContent('es', webPartName, es_elem, true);
 
             // Cancel and Save Tabs Button
             $('#save').button().click(function(event) {
@@ -115,6 +111,11 @@
 
         var _removeTabs = function() {
 
+            // clean up CKEditors
+            for(var instanceName in CKEDITOR.instances) {
+                CKEDITOR.instances[instanceName].destroy(true);
+            }
+
             // Clean up tabs
             var tabs = document.getElementById('tabs');
             $(tabs).tabs('destroy');
@@ -126,7 +127,7 @@
             content.removeAttribute('id');
 
             // Add our click handler
-            enable_inline(content);
+            _enable_inline(content);
 
             // Swap elements
             parent.replaceChild(content, tabs);
@@ -140,14 +141,13 @@
                 CKEDITOR.config.stylesSet = 'CM Styles';
 
                 $('.editable').each(function(index, elem) {
-                    enable_inline(elem);
+                    _enable_inline(elem);
                 });
             });
         };
 
         return {
-            init:init,
-            enable: enable_inline
+            init:init
         };
     };
 }());
