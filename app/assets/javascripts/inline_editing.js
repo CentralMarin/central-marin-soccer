0f3ad8ba2845ab8f37a4bbefb833fb9acefd3e5f
Ryan Robinett <ryan@digitalfoundry.com>
2015-09-02 10:13:45 -0700

    Now uses a JavaScript template for HTML

--- app/assets/javascripts/inline_editing.js
+++ app/assets/javascripts/inline_editing.js
@@ -4,7 +4,7 @@
     namespace("soccer");
     soccer.inline_editing = function() {
 
-        var tabsHTML = "<ul><li><a href='#tabs-english'>English</a></li><li><a href='#tabs-spanish'>Spanish</a></li></ul><div id='tabs-spanish'><p>SPANISH</p></div><span style='position: absolute; right: 5px; top: 10px'><a href='#' id='save'>Save</a><a href='#' id='cancel'>Cancel</a></a></span>";
+        var tabsHTML = $('#tabsTemplate').render({});
 
         var _enable_inline = function(elem) {
             var $elem = $(elem);
