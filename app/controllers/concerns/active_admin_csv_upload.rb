e3c0627f1115c09a889cc2e14d04b077122fc0b1
Ryan Robinett <ryan@digitalfoundry.com>
2015-09-04 11:59:25 -0700

    removed unused method

--- app/controllers/concerns/active_admin_csv_upload.rb
+++ app/controllers/concerns/active_admin_csv_upload.rb
@@ -32,18 +32,4 @@ module ActiveAdminCsvUpload
     end
 
   end
-
-  def import_csv_file(csv_data)
-    transaction do
-      # remove all the existing records
-      destroy_all
-
-      # read the csv
-      csv_file = csv_data.read
-      CSV.parse(csv_file, {:headers => true}) do |row|
-        yield new(), row
-      end
-
-    end
-  end
 end
