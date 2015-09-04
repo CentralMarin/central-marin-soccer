f66de32011b357771617f8b5110461bea091716d
Ryan Robinett <ryan@digitalfoundry.com>
2015-09-03 18:30:01 -0700

    Initial version of translate

---
+++ app/controllers/concerns/active_admin_translate.rb
@@ -0,0 +1,35 @@
+module ActiveAdminTranslate
+
+  # Shared member actions. Using "send" due to "_action" and "_item" are a private methods
+  def self.included(base)
+
+    base.send(:collection_action, :translate, :method => :get) do
+      id = params[:id]
+      model =  Article.find_by(id: id)
+      fields = translation_fields
+
+      translator = BingTranslator.new(Rails.application.secrets.bing_client_id, Rails.application.secrets.bing_client_secret)
+
+      english_translations = model.translations.find_by(locale: :en)
+      spanish_translation = model.translations.find_by(locale: :es)
+
+      fields.each do |field|
+        content = english_translations.send(field)
+        spanish = translator.translate(content, from: 'en', to: 'es')
+
+        spanish_translation.send("#{field}=", spanish)
+      end
+
+      status = spanish_translation.save
+
+      flash[:notice] = 'Content Translated!'
+      redirect_to action: :show, id: id
+    end
+
+    base.send(:action_item, :only => :show) do
+      link_to 'Translate to Spanish', :action => 'translate', id: params[:id]
+    end
+
+  end
+
+end
