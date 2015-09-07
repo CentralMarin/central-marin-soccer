e30b1f7a7c7668b760f23468dd2437b2a24140f1
Ryan Robinett <ryan@digitalfoundry.com>
2015-09-06 22:12:22 -0700

    Now saves the filename as the MD5 hash

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -8,22 +8,19 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :file
 
+  def md5
+    chunk = model.send(mounted_as)
+    @md5 ||= Digest::MD5.hexdigest(chunk.read.to_s)
+  end
+
   def filename
-    if (defined? model.image_base_filename).nil? or file.nil?
-      super
-    else
-      "#{model.image_base_filename}.#{file.extension}"
-    end
+    @name ||= "#{md5}#{File.extname(super)}" if super
   end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    if (defined? model.image_store_dir).nil?
-      "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    else
-      "uploads/#{model.image_store_dir}"
-    end
+    "uploads/"
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
