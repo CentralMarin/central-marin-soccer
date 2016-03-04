c6551a7e375eef0d5fe6aa1ee50e9789fa27de30
Ryan Robinett <ryan@digitalfoundry.com>
2016-03-04 08:30:55 -0800

    Initial version of welcome email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,8 +29,6 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-  config.action_mailer.default_url_options = { :host => "localhost:3000", :protocol => 'http' }
-
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :letter_opener_web
