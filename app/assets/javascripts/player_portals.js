2683ed4dea98e99919555676d4ed6f6c4190c8e8
Ryan Robinett <ryan@digitalfoundry.com>
2016-03-08 09:38:19 -0800

    fixed reloading of pdfs

--- app/assets/javascripts/player_portals.js
+++ app/assets/javascripts/player_portals.js
@@ -9,6 +9,17 @@
 //= require jquery.Jcrop.min
 //= require namespace
 
+function makeid()
+{
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 5; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+
 (function () {
     namespace('player_portal');
     player_portal.modalPdf = (function() {
@@ -17,18 +28,23 @@
             // setup document modal
             $('#documentModal').on('show.bs.modal', function (event) {
                 var button = $(event.relatedTarget); // button that triggered the modal
-                var document = button.data('document');
                 var title = button.data('title');
                 var modal = $(this);
+                var document = button.data('document') + '?id=' + makeid();
+
+                // force the browser to reload the pdf by adding a random querystring
+
+
+                // Set the document links
+                var obj = modal.find('.modal-body object');
+                obj.attr('data', document);
+                obj.find('a').attr('href', document);
 
                 // Set the title
                 modal.find('.modal-title').text(title);
 
-                // Set the document links
+                // Set the footer
                 modal.find('.modal-footer a').attr('href', document);
-                var obj = modal.find('.modal-body object');
-                obj.attr('data', document);
-                obj.find('a').attr('href', document);
             });
         };
 
