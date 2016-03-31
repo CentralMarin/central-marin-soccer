0f02411cde5f78df27e8e82325de61ad0235894f
Ryan Robinett <ryan@digitalfoundry.com>
2016-03-31 12:29:54 -0700

    added constant

--- app/controllers/concerns/active_admin_csv_controller.rb
+++ app/controllers/concerns/active_admin_csv_controller.rb
@@ -1,13 +1,16 @@
 module ActiveAdminCsvController
 
-    def download_csv(models)
-      file = CSV.generate do |csv|
-        generate_csv(csv, models)
-      end
+  CSV_ENCODING = 'UTF-8'
 
-      send_data file, :type => 'text/csv; charset=UTF-8; header=present', :disposition => "attachment;filename=#{resource_class.to_s.pluralize}.csv"
+  def download_csv(models)
 
+    file = CSV.generate do |csv|
+      generate_csv(csv, models)
     end
+
+    send_data file, :type => "text/csv; charset=#{CSV_ENCODING}; header=present", :disposition => "attachment;filename=#{resource_class.to_s.pluralize}.csv"
+
+  end
 
 
 end
