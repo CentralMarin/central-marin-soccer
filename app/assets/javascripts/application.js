421506b1afa19d160b43b94a4622d74f7e51badc
Ryan Robinett <ryan@digitalfoundry.com>
2016-05-06 08:48:14 -0700

    Fixed jquery bug w/ polyfill

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,3 +7,20 @@
 
 //= require jquery
 //= require jquery_ujs
+
+jQuery.swap = function( elem, options, callback, args ) {
+    var ret, name, old = {};
+    // Remember the old values, and insert the new ones
+    for ( name in options ) {
+        old[ name ] = elem.style[ name ];
+        elem.style[ name ] = options[ name ];
+    }
+
+    ret = callback.apply( elem, args || [] );
+
+    // Revert the old values
+    for ( name in options ) {
+        elem.style[ name ] = old[ name ];
+    }
+    return ret;
+};
