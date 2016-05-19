f0ac6a0b1f57bcffd10c6d0c5a286a724176a1b1
Ryan Robinett <ryan@digitalfoundry.com>
2016-05-19 06:59:02 -0700

    Added view into notifications

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,28 +3,17 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    section "Bounded Messages" do
-      mg_client = Mailgun::Client.new
-      result = mg_client.get "#{mailgun_domain}/stats"
-      i = 0
+
+    columns do
+      column do
+        panel "Notifications" do
+          mg_client = Mailgun::Client.new
+          results = mg_client.get "#{Rails.application.secrets.mailgun_domain}/stats"
+
+          render partial: 'notifications', locals: {stats: results.to_h}
+        end
+      end
     end
-    #
-    # section "Players" do
-    #   ul do
-    #     li do
-    #       jobs = Delayed::Job.where('failed_at is not null').count(:id)
-    #       link_to "#{jobs} failing jobs", admin_jobs_path(q: {failed_at_is_not_null: true}), style: 'color: red'
-    #     end
-    #     li do
-    #       jobs = Delayed::Job.where('run_at <= ?', now).count(:id)
-    #       link_to "#{jobs} late jobs", admin_jobs_path(q: {run_at_lte: now.to_s(:db)}), style: 'color: hsl(40, 100%, 40%)'
-    #     end
-    #     li do
-    #       jobs = Delayed::Job.where('run_at > ?', now).count(:id)
-    #       link_to "#{jobs} scheduled jobs", admin_jobs_path(q: {run_at_gte: now.to_s(:db)}), style: 'color: green'
-    #     end
-    #   end
-    # end
 
     # Here is an example of a simple dashboard with columns and panels.
     #
