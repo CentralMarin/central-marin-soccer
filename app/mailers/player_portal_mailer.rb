886c208b8ac800f49920f887b2a466dddc67e960
Ryan Robinett <ryan@digitalfoundry.com>
2017-03-13 11:04:21 -0700

    Swapped out ActionMailer for MailGun

--- app/mailers/player_portal_mailer.rb
+++ app/mailers/player_portal_mailer.rb
@@ -8,7 +8,7 @@ class PlayerPortalMailer < ApplicationMailer
     if player_portal.email.present? || player_portal.parent1_email.present? || player_portal.parent2_email.present?
 
       mail to: [player_portal.email, player_portal.parent1_email, player_portal.parent2_email],
-           from: Rails.application.secrets.google_email_from,
+           from: ENV['MAILGUN_FROM'],
            subject: "Register now for Central Marin! / Inscribense ahora para Central Marin!"
     end
   end
